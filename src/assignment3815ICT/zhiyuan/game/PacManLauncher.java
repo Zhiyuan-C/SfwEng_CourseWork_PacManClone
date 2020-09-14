65d61b457077bb10f6c26977d3a27f65bc8d79fd
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-14 12:03:16 +1000

    setup launcher

--- src/assignment3815ICT/zhiyuan/game/PacManLauncher.java
+++ src/assignment3815ICT/zhiyuan/game/PacManLauncher.java
@@ -2,6 +2,11 @@ package assignment3815ICT.zhiyuan.game;
 
 public class PacManLauncher {
 
+    public PacManLauncher() {
+        new GameWindow();
+    }
+
     public static void main(String[] args) {
+        new PacManLauncher();
     }
 }
