df3c4e5ea26f32fd2660443fd17dd82946cbdffd
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-14 13:43:51 +1000

    initialise class

--- src/assignment3815ICT/zhiyuan/game/states/Game.java
+++ src/assignment3815ICT/zhiyuan/game/states/Game.java
@@ -1,4 +1,18 @@
 package assignment3815ICT.zhiyuan.game.states;
 
-public class Game {
+import assignment3815ICT.zhiyuan.game.userInteractions.KeyHandler;
+
+import java.awt.*;
+
+public abstract class Game {
+
+    private StateManager stateManager;
+
+    public Game(StateManager stateManager){
+        this.stateManager = stateManager;
+    }
+
+    public abstract void update();
+    public abstract void input(KeyHandler keyHandler);
+    public abstract void render(Graphics2D graphics2D);
 }
