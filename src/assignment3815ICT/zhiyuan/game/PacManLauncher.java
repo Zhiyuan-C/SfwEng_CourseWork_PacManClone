5765df280e91314bfe05dbaf4ade8913e182e91c
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-14 22:05:59 +1000

    modify content

--- src/assignment3815ICT/zhiyuan/game/PacManLauncher.java
+++ src/assignment3815ICT/zhiyuan/game/PacManLauncher.java
@@ -1,9 +1,8 @@
 package assignment3815ICT.zhiyuan.game;
 
-
 public class PacManLauncher {
 
     public static void main(String[] args) {
-
+        new Game("Pac-Man", 1280, 720);
     }
 }
--- src/assignment3815ICT/zhiyuan/game/display/GameWindow.java
+++ src/assignment3815ICT/zhiyuan/game/display/GameWindow.java
@@ -1,10 +1,12 @@
 package assignment3815ICT.zhiyuan.game.display;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameWindow {
 
     private JFrame frame;
+    private Canvas canvas;
     private String title;
     private int width, height;
 
@@ -22,5 +24,13 @@ public class GameWindow {
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(width, height));
+        canvas.setMaximumSize(new Dimension(width, height));
+        canvas.setMinimumSize(new Dimension(width, height));
+
+        frame.add(canvas);
+        frame.pack();
     }
 }
