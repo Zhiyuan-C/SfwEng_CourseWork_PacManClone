9d356e80563cd2c89c6413a48c6db3b08dd96cda
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-14 16:11:06 +1000

    add game over state

---
+++ src/assignment3815ICT/zhiyuan/game/states/GameOver.java
@@ -0,0 +1,26 @@
+package assignment3815ICT.zhiyuan.game.states;
+
+import assignment3815ICT.zhiyuan.game.userInteractions.KeyHandler;
+
+import java.awt.*;
+
+public class GameOver extends State {
+    public GameOver(StateManager stateManager) {
+        super(stateManager);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void input(KeyHandler keyHandler) {
+
+    }
+
+    @Override
+    public void render(Graphics2D graphics2D) {
+
+    }
+}
