0f73375c77104b72ecdb90072228125ac667b2cf
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-15 16:39:56 +1000

    tested if new entity works or not

--- src/assignment3815ICT/zhiyuan/game/states/PlayState.java
+++ src/assignment3815ICT/zhiyuan/game/states/PlayState.java
@@ -1,22 +1,26 @@
 package assignment3815ICT.zhiyuan.game.states;
 
+import assignment3815ICT.zhiyuan.game.entities.mob.PacMan;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class PlayState extends State {
 
+    private PacMan pacMan;
+
     // constructor
     public PlayState() {
-
+        pacMan = new PacMan(10, 10);
     }
 
     @Override
     public void update() {
-
+        pacMan.update();
     }
 
     @Override
     public void render(Graphics graphics, BufferedImage gameObject) {
-        graphics.drawImage(gameObject, 0, 0, null);
+        pacMan.render(graphics, gameObject);
     }
 }
