a39cb9cbb1d93c640651a7ca5df585d3f79dfef4
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-15 16:34:49 +1000

    initialise

--- src/assignment3815ICT/zhiyuan/game/entities/mob/Mob.java
+++ src/assignment3815ICT/zhiyuan/game/entities/mob/Mob.java
@@ -1,4 +1,9 @@
 package assignment3815ICT.zhiyuan.game.entities.mob;
 
-public class Mob {
+import assignment3815ICT.zhiyuan.game.entities.Entity;
+
+public abstract class Mob extends Entity {
+    public Mob(float x, float y) {
+        super(x, y);
+    }
 }
