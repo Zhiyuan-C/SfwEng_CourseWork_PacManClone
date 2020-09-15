473b2ed3bb54f9d64f1dff0cfcc4336bb152c789
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-15 15:38:42 +1000

    initialise class

--- src/assignment3815ICT/zhiyuan/game/states/StateManager.java
+++ src/assignment3815ICT/zhiyuan/game/states/StateManager.java
@@ -1,4 +1,13 @@
 package assignment3815ICT.zhiyuan.game.states;
 
 public class StateManager {
+    private static State currentState = null;
+
+    public static void setState(State state) {
+        currentState = state;
+    }
+
+    public static State getCurrentState() {
+        return currentState;
+    }
 }
