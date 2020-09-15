c93fb36ced2818206321fae4579706523108b1f8
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-15 16:35:54 +1000

    initialise

--- src/assignment3815ICT/zhiyuan/game/entities/mob/PacMan.java
+++ src/assignment3815ICT/zhiyuan/game/entities/mob/PacMan.java
@@ -1,4 +1,20 @@
 package assignment3815ICT.zhiyuan.game.entities.mob;
 
-public class PacMan {
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class PacMan extends Mob {
+    public PacMan(float x, float y) {
+        super(x, y);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Graphics graphics, BufferedImage image) {
+        graphics.drawImage(image, (int) x, (int) y, null);
+    }
 }
