bdf3984de920b57d62f0a0ddd1d4d6fce74d6aa4
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-15 16:34:15 +1000

    initialise

--- src/assignment3815ICT/zhiyuan/game/entities/Entity.java
+++ src/assignment3815ICT/zhiyuan/game/entities/Entity.java
@@ -1,4 +1,19 @@
 package assignment3815ICT.zhiyuan.game.entities;
 
-public class Entity {
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public abstract class Entity {
+
+    protected float x, y; // x position and y position
+
+    // constructor
+    public Entity(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    //abstract methods
+    public abstract void update();
+    public abstract void render(Graphics graphics, BufferedImage image);
 }
