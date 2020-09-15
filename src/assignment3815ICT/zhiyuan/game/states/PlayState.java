f1ec75aa90ff0bc20616c0a95ae3866a7839e09f
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-15 15:38:47 +1000

    initialise class

--- src/assignment3815ICT/zhiyuan/game/states/PlayState.java
+++ src/assignment3815ICT/zhiyuan/game/states/PlayState.java
@@ -1,4 +1,22 @@
 package assignment3815ICT.zhiyuan.game.states;
 
-public class PlayState {
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class PlayState extends State {
+
+    // constructor
+    public PlayState() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Graphics graphics, BufferedImage gameObject) {
+        graphics.drawImage(gameObject, 0, 0, null);
+    }
 }
