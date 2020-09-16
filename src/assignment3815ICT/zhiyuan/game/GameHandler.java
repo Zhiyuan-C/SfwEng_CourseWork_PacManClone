6b9217a45859b3be3e342db3fb09d2a14d77c7c6
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-16 13:03:09 +1000

    add and intialise game handler to pass variables

---
+++ src/assignment3815ICT/zhiyuan/game/GameHandler.java
@@ -0,0 +1,41 @@
+package assignment3815ICT.zhiyuan.game;
+
+import assignment3815ICT.zhiyuan.game.map.Map;
+import assignment3815ICT.zhiyuan.game.userInputs.KeyManager;
+
+public class GameHandler {
+    private Game game;
+    private Map map;
+
+    public GameHandler(Game game) {
+        this.game = game;
+    }
+
+    public int getGameWidth() {
+        return game.getWidth();
+    }
+
+    public int getGameHeight() {
+        return game.getHeight();
+    }
+
+    public KeyManager getKeyManager() {
+        return game.getKeyManager();
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+}
