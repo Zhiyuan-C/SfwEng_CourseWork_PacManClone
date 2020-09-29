887b698792f54f2fddf666aefebba1eb755c869d
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-29 11:44:05 +1000

    initialise items

--- src/assignment3815ICT/zhiyuan/game/entities/item/FlowerSmall.java
+++ src/assignment3815ICT/zhiyuan/game/entities/item/FlowerSmall.java
@@ -1,4 +1,26 @@
 package assignment3815ICT.zhiyuan.game.entities.item;
 
-public class FlowerSmall {
+import assignment3815ICT.zhiyuan.game.GameHandler;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class FlowerSmall extends Item{
+
+    private BufferedImage flowerSmall;
+
+    public FlowerSmall(GameHandler gameHandler, float xPos, float yPos) {
+        super(gameHandler, xPos, yPos, 32, 32);
+        this.flowerSmall = itemImages.get(0);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+        graphics.drawImage(flowerSmall, (int)xPos, (int)yPos, width, height, null);
+    }
 }
--- src/assignment3815ICT/zhiyuan/game/entities/item/Item.java
+++ src/assignment3815ICT/zhiyuan/game/entities/item/Item.java
@@ -1,4 +1,16 @@
 package assignment3815ICT.zhiyuan.game.entities.item;
 
-public class Item {
+import assignment3815ICT.zhiyuan.game.GameHandler;
+import assignment3815ICT.zhiyuan.game.entities.Entity;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public abstract class Item extends Entity {
+    protected ArrayList<BufferedImage> itemImages;
+
+    public Item(GameHandler gameHandler, float xPos, float yPos, int width, int height) {
+        super(gameHandler, xPos, yPos, width, height);
+        this.itemImages = gameHandler.getItemObjects();
+    }
 }
