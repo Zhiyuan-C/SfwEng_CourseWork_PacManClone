26049df61437ea1e839a6750f67df49ecd413620
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-09-29 11:53:58 +1000

    remove pacman class from play state

--- src/assignment3815ICT/zhiyuan/game/states/PlayState.java
+++ src/assignment3815ICT/zhiyuan/game/states/PlayState.java
@@ -9,7 +9,6 @@ import java.awt.image.BufferedImage;
 
 public class PlayState extends State {
 
-    private PacMan pacMan;
     private Map map;
 
     // constructor
@@ -17,20 +16,15 @@ public class PlayState extends State {
         super(gameHandler);
         map = new Map(gameHandler,"res/map.txt");
         gameHandler.setMap(map);
-        pacMan = new PacMan(gameHandler, 645, 356);
-
     }
 
     @Override
     public void update() {
         map.update();
-        pacMan.update();
     }
 
     @Override
     public void render(Graphics graphics) {
         map.render(graphics);
-        pacMan.render(graphics);
-//        pacMan.move();
     }
 }
