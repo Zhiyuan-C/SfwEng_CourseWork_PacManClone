33b4c860cac5ed6357b533242141b38a4980813c
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-10-01 16:09:09 +1000

    create font class to display font fron spritesheet

---
+++ src/assignment3815ICT/zhiyuan/game/graphics/display/GameFont.java
@@ -0,0 +1,33 @@
+package assignment3815ICT.zhiyuan.game.graphics.display;
+
+import assignment3815ICT.zhiyuan.game.graphics.tiles.Tile;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class GameFont {
+    private String characters =""+
+            "ABCDEFGHI" +
+            "JKLMNOPQR" +
+            "STUVWXYZ " +
+            "abcdefghi" +
+            "jklmnopqr" +
+            "stuvwxyz " +
+            "!,-./<=>?" +
+            "012345678" +
+            "9        ";
+    private ArrayList<BufferedImage> fontObjects;
+
+    public GameFont(ArrayList<BufferedImage> fontObjects) {
+        this.fontObjects = fontObjects;
+    }
+
+    public void render(Graphics graphics, String message, int xPos, int yPos, int width, int height) {
+        for(int i = 0; i < message.length(); i ++){
+            int charIndex = characters.indexOf(message.charAt(i));
+            if(charIndex >= 0) graphics.drawImage(fontObjects.get(charIndex), xPos + i * width, yPos, width, height, null);
+        }
+    }
+
+}
