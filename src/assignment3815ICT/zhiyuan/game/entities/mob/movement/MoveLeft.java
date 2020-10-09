2e632c1a1e620d1606873c38a12fb57a9fd109f4
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-10-09 14:43:25 +1000

    create class for move left and right

---
+++ src/assignment3815ICT/zhiyuan/game/entities/mob/movement/MoveLeft.java
@@ -0,0 +1,36 @@
+package assignment3815ICT.zhiyuan.game.entities.mob.movement;
+
+import assignment3815ICT.zhiyuan.game.entities.mob.ghosts.Ghost;
+
+public class MoveLeft extends Move{
+    public MoveLeft(Ghost ghost, int mapTileWidth, int mapTileHeight, int tileWidth, int tileHeight) {
+        super(ghost, mapTileWidth, mapTileHeight, tileWidth, tileHeight);
+    }
+
+    @Override
+    protected void checkMove(float xPos, float yPos, float targetX, float targetY, int delta) {
+
+    }
+
+    @Override
+    protected void checkMovableTiles(int delta) {
+
+    }
+
+    @Override
+    public void checkMovable(float xPos, float yPos, float targetX, float targetY) {
+//        this.xPos = xPos;
+//        this.yPos = yPos;
+//        currentTileX = (int) xPos / tileWidth;
+//        currentTileY = (int) yPos / tileHeight;
+//
+//        int newTileX;
+//        newTileX = currentTileX + delta;
+//
+//        offsetX = newTileX * tileWidth - xPos;
+//        offsetY = 0;
+//
+//        movable = !ghost.isWallCollide(offsetX, offsetY);
+//        checkingLength = currentTileX + (delta * 2);
+    }
+}
---
+++ src/assignment3815ICT/zhiyuan/game/entities/mob/movement/MoveRight.java
@@ -0,0 +1,4 @@
+package assignment3815ICT.zhiyuan.game.entities.mob.movement;
+
+public class MoveRight {
+}
