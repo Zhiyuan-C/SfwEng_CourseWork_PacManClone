1a32e3c6c1173c30932b5103881d00f987449902
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-10-10 19:55:01 +1000

    finish entity test

--- test/assignment3815ICT/zhiyuan/game/entities/EntityTest.java
+++ test/assignment3815ICT/zhiyuan/game/entities/EntityTest.java
@@ -4,6 +4,9 @@ import assignment3815ICT.zhiyuan.game.Game;
 import assignment3815ICT.zhiyuan.game.GameHandler;
 import assignment3815ICT.zhiyuan.game.entities.item.FlowerLarge;
 import assignment3815ICT.zhiyuan.game.entities.item.Item;
+import assignment3815ICT.zhiyuan.game.entities.item.Wall;
+import assignment3815ICT.zhiyuan.game.entities.mob.ghosts.Blinky;
+import assignment3815ICT.zhiyuan.game.entities.mob.ghosts.Ghost;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -39,14 +42,30 @@ public class EntityTest {
     public void mobCollisions() {
         // set position pacman and blinky same so they will collide
         gameHandler.setPlayState();
+        ArrayList<Ghost> ghosts = new ArrayList<>();
+        Blinky blinky = new Blinky(gameHandler, 50, 60);
+        ghosts.add(blinky);
+
         gameHandler.getMap().getEntityManager().getPacMan().setxPos(50);
         gameHandler.getMap().getEntityManager().getPacMan().setyPos(60);
-        gameHandler.getMap().getEntityManager().getGhosts().get(0).setxPos(50);
-        gameHandler.getMap().getEntityManager().getGhosts().get(0).setxPos(60);
+        gameHandler.getMap().getEntityManager().setGhosts(ghosts);
+        // if collide pac-man alive should be false
+        gameHandler.getMap().getEntityManager().getPacMan().mobCollisions(0, 0);
+        assertFalse(gameHandler.getMap().getEntityManager().getPacMan().isAlive());
 
     }
 
     @Test
     public void isWallCollide() {
+        gameHandler.setPlayState();
+        ArrayList<Item> walls = new ArrayList<>();
+        Wall wall = new Wall(gameHandler, 78, 78);
+        walls.add(wall);
+
+        gameHandler.getMap().getEntityManager().getPacMan().setxPos(78);
+        gameHandler.getMap().getEntityManager().getPacMan().setyPos(78);
+        gameHandler.getMap().getEntityManager().setWalls(walls);
+
+        assertTrue(gameHandler.getMap().getEntityManager().getPacMan().isWallCollide(0, 0));
     }
 }
