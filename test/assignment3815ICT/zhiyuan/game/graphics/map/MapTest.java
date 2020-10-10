d02e6c2cf075cc4e3425970cafb22b671df0a4a5
zhiyuan <zhiyuan.chen@griffithuni.edu.au>
2020-10-10 21:14:21 +1000

    some changes

---
+++ test/assignment3815ICT/zhiyuan/game/graphics/map/MapTest.java
@@ -0,0 +1,12 @@
+package assignment3815ICT.zhiyuan.game.graphics.map;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MapTest {
+
+    @Test
+    public void getTile() {
+    }
+}
